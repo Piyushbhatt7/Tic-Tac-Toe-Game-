7d351c80fc9a922b8c094aae19d5096efe9a55e6
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-06 23:02:32 +0530

    tic tack toe game

--- src/App.java
+++ src/App.java
@@ -2,6 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         char[][] board = new char[3][3];
-        
+        for(int rows = 0; rows < board.length; rows++)
+        {
+            for(int col = 0; col < board.length; col++)
+            {
+                
+            }
+        }
     }
 }
