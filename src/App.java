fc546f5026c404128077b8cccec213a0427255c7
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-07 11:52:02 +0530

    Tic Tak Toe game

--- src/App.java
+++ src/App.java
@@ -32,7 +32,7 @@ public class App {
             }
             if(gameOver)
             {
-                System.out.println("Player " + player + " has won 🎉🎉");
+                System.out.println("Player " + player + " has won!");
             }
             else {
                 if(player == 'X')
