ab598bc1121b78d9ea61ff4900d1334e48b5a750
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-14 21:09:30 +0530

    Tictak Toe game

--- src/App.java
+++ src/App.java
@@ -51,7 +51,7 @@ public class App {
 
     public static boolean haveWon(char[][] board, char player)
     {
-      // check rows    
+      // check rows     
       for(int rows = 0; rows < board.length; rows++)
       {
          if(board[rows][0] == player && board[rows][1] == player && board[rows][2] == player)
